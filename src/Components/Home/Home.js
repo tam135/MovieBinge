ee42f9f67e1f2cfb32db51f2ff4f7085f351ed1a
Tam Phan <tnphan0204@gmail.com>
2019-09-17 15:48:51 -0400

    incorporating HeroImage with props thru home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -29,12 +29,52 @@ class Home extends Component {
   componentDidMount() {
     this.setState({ loading: true });
     const endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=1`;
+    this.fetchItems(endpoint);
   }
+
+  loadMoreItems = () => {
+    let endpoint = "";
+    this.setState({
+      loading: true
+    });
+
+    if (this.state.searchTerm === "") {
+      endpoint = `${API_URL}movie/popular?api_key=${API_KEY}&language=en-US&page=${this
+        .state.currentPage + 1}`;
+    } else {
+      endpoint = `${API_URL}search/movie?api_key=${API_KEY}&language=en-US&query${
+        this.state.searchTerm
+      }$page=${this.state.currentPage + 1}`;
+    }
+    this.fetchItems(endpoint);
+  };
+
+  fetchItems = endpoint => {
+    fetch(endpoint)
+      .then(result => result.json())
+      .then(result => {
+        this.setState({
+          movies: [...this.state.movies, ...result.results],
+          heroImage: this.state.heroImage || result.results[0],
+          loading: false,
+          currentPage: result.page,
+          totalPages: result.total_pages
+        });
+      });
+  };
   render() {
     return (
       <div className="rmdb-home">
-        <HeroImage />
-        <SearchBar />
+        {this.state.heroImage ? (
+          <div>
+            <HeroImage
+              image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${this.state.heroImage.backdrop_path}`}
+              title={this.state.heroImage.original_title}
+              text={this.state.heroImage.overview}
+            />
+            <SearchBar />
+          </div>
+        ) : null}
         <FourColGrid />
         <Spinner />
         <LoadMoreBtn />
