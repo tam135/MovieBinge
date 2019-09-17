0bea4c80844a0bf023013d4555d4752cc9ea2acb
Tam Phan <tnphan0204@gmail.com>
2019-09-17 15:40:49 -0400

    rendered home and header in app

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+import Header from "../elements/Header/Header";
+import Home from "../Home/Home";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Home />
+        <Header />
+      </div>
+    );
+  }
 }
 
 export default App;
